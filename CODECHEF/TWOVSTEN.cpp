0ed2b82718f6e42d0d7ac99ae7bc9627b4f57ca0
Omkar Rane <omkarrane10@gmail.com>
2019-09-09 03:21:51 +0530

    CODECHEF-TWOVSTEN

---
+++ CODECHEF/TWOVSTEN.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n;
+    cin >> n;
+    if((n % 10) == 0)
+      cout << "0" << endl;
+    else if((n % 10) == 5)
+      cout << "1" << endl;
+    else
+      cout << "-1" << endl;
+  }
+  return 0;
+}
