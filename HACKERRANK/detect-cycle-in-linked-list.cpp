8a57eda2653321c62ea447f67538f0a38b4a537b
Omkar Rane <omkarrane10@gmail.com>
2019-10-11 01:15:40 +0530

    HACKERRANK-detect cycle in linked list using 2 methods - set and floyds cycle detection algorithm

---
+++ HACKERRANK/detect-cycle-in-linked-list.cpp
@@ -0,0 +1,154 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the has_cycle function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool has_cycle_using_set(SinglyLinkedListNode* head) {
+    set<SinglyLinkedListNode*> s;
+    while(true && head != nullptr)
+    {
+        auto f = s.find(head);
+
+        if(f != s.end())
+            return true;
+
+        s.insert(head);
+        head = head->next;
+    }
+
+    return false;
+}
+
+bool has_cycle_using_floyds_cycle_detection(SinglyLinkedListNode* head) {
+    SinglyLinkedListNode* slow = head;
+    SinglyLinkedListNode* fast = head;
+    while(slow != nullptr && fast != nullptr && fast->next != nullptr)
+    {
+      fast = fast->next->next;
+      slow = slow->next;
+
+      if(fast == slow)
+        return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int tests;
+    cin >> tests;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+        int index;
+        cin >> index;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        SinglyLinkedList* llist = new SinglyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist->insert_node(llist_item);
+        }
+      
+      	SinglyLinkedListNode* extra = new SinglyLinkedListNode(-1);
+      	SinglyLinkedListNode* temp = llist->head;
+      
+      	for (int i = 0; i < llist_count; i++) {
+            if (i == index) {
+          		extra = temp;
+            }
+          	
+          	if (i != llist_count-1) {
+          		temp = temp->next;
+            }
+        }
+      
+      	temp->next = extra;
+
+        bool result1 = has_cycle_using_floyds_cycle_detection(llist->head);
+        bool result2 = has_cycle_using_set(llist->head);
+
+        fout << result1 << "\n";
+        fout << result2 << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
