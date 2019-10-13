388bbf9bafdd72dbe8b69d4583e9be7ac5f3a813
Omkar Rane <omkarrane10@gmail.com>
2019-10-13 23:54:26 +0530

    HACKERRANK-started working on sherlock and valid string

---
+++ HACKERRANK/sherlock-and-valid-string.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the isValid function below.
+string isValid(string s) {
+  
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = isValid(s);
+
+    cout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
