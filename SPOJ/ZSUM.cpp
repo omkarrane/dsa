e3bb4ee7fdfbc8a64060b47fbfc4231a7cf277a2
Omkar Rane <omkarrane10@gmail.com>
2019-10-19 23:52:11 +0530

    SPOJ-ZSUM, not tested

---
+++ SPOJ/ZSUM.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+#define m 10000007
+#define ll long long
+#define l long
+
+ll bin_expo(ll n, l k)
+{
+  ll res;
+  return n % m;
+}
+
+ll calc_s(ll n, l k)
+{
+
+}
+
+ll calc_p(ll n, l k)
+{
+
+}
+
+int main()
+{
+  ll n;
+  l k;
+  while(cin >> n >> k && n != 0 && k != 0)
+  {
+
+  }
+}
