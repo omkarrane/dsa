d06f70d0f63368ba7a0225334db9e02bbba411d3
Omkar Rane <omkarrane10@gmail.com>
2019-11-16 22:52:21 +0530

    GFG-two-dimensional-world

---
+++ GEEKSFORGEEKS/two-dimensional-world.cpp
@@ -0,0 +1,53 @@
+//Initial Template for C++
+#include <iostream>
+using namespace std;
+//Position this line where user code will be pasted.
+// Driver code
+/*This is a function problem.You only need to complete the function given below*/
+//User function Template for C++
+/* Function to take input for 2D array elements
+* Note : Mention matrix in argument also.
+* N : size of matrix
+*/
+void twoDimensional(int** mat,int N){
+    // Loop to iterate through matrix
+    for(int i = 0;i<N;i++){
+        for(int j = 0;j<N;j++){
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+}
+
+int main() {
+	
+	int testcase;
+	cin >> testcase;
+	
+	while(testcase--){
+	    int N;
+	    cin >> N;
+	    
+	    // Declaring mat as pointer to pointer
+	    int **mat;
+	
+	    // mat pointer contains array of pointer to array
+    	mat = new int*[N];
+    	
+    	// Taking input to mat[][]
+    	for(int i = 0;i<N;i++){
+    	    mat[i] = new int[N];
+    	}
+    	
+    	for(int i = 0;i<N;i++){
+    	    for(int j = 0;j<N;j++){
+    	        cin >> mat[i][j];
+    	    }
+    	}
+    	
+    	twoDimensional(mat, N);   
+	}
+	
+	return 0;
+}
