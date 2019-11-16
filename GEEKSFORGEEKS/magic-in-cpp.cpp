b0338af35063a7c898264db72e028b30d69d5e7c
Omkar Rane <omkarrane10@gmail.com>
2019-11-16 22:38:59 +0530

    GFG-magic-in-cpp

---
+++ GEEKSFORGEEKS/magic-in-cpp.cpp
@@ -0,0 +1,45 @@
+//Initial Template for C++
+#include <iostream>
+using namespace std;
+// Function prototype
+void swap(int*, int*);
+void swap(int&, int&);
+//Position this line where user code will be pasted.
+// Driver code
+int main() {
+	
+	int testcase;
+	cin >> testcase;
+	
+	while(testcase--){
+	    int A, B;
+	    cin >> A >> B;
+	    
+	    // Swapping by reference
+	    swap(A, B);
+	    cout << A << " " << B << endl;
+	    
+	    // Swap by pointers
+	    swap(&A, &B);
+	    cout << A << " " << B << endl;
+	}
+	
+	return 0;
+}
+
+/*This is a function problem.You only need to complete the function given below*/
+//User function Template for C++
+// Function to swap A and B
+// using pointers
+void swap(int *P, int *Q){
+  int R = *P;
+  *P = *Q;
+  *Q = R;
+}
+// Function to swap A and B
+// using reference
+void swap(int &A, int &B){
+  int C = A;
+  A = B;
+  B = C;  
+}
