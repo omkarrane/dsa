9ff038f24140a299cf0abde352b1baf519e3b15e
Omkar Rane <omkarrane10@gmail.com>
2019-11-28 01:30:32 +0530

    GFG-number of occurence

---
+++ GEEKSFORGEEKS/number-of-occurence.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define s_int(num) scanf("%d", &num)
+
+int compare(const void* ap, const void* bp) 
+{ 
+    // Typecasting 
+    const int* a = (int*)ap; 
+    const int* b = (int*)bp; 
+  
+    if (*a < *b) 
+        return -1; 
+    else if (*a > *b) 
+        return 1; 
+    else
+        return 0; 
+}
+
+int main()
+{
+  int t;
+  s_int(t);
+
+  while(t--)
+  {
+    int n, x;
+    s_int(n);
+    s_int(x);
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+      s_int(arr[i]);
+
+    int* p = (int *)bsearch(&x, arr, n, sizeof(arr[0]), compare);
+    if(!p)
+    {
+      printf("-1\n");
+      continue;
+    }
+
+    int i = (p - arr);
+    int temp1 = i, temp2 = i;
+    while(temp1 != 0 && arr[temp1] == arr[temp1-1])
+      temp1--;
+    
+    while(temp2 != n-1 && arr[temp2] == arr[temp2+1])
+      temp2++;
+
+    printf("%d\n", (temp2 - temp1 + 1));
+  }
+
+  return 0;
+}
