97b9932e68e485b2969b8c993380656dfd464574
Omkar Rane <omkarrane10@gmail.com>
2020-06-06 19:42:14 +0530

    CODEFORCES-domino piling

---
+++ CODEFORCES/domino-piling.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define s_int(num) scanf("%d", &num)
+
+int main()
+{
+  int a, b;
+  s_int(a);
+  s_int(b);
+  cout << int(a * b / 2) << "\n";
+
+  return 0;
+}
