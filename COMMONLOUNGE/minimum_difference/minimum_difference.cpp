02c238ff2998eeb263c391f414ffcb7b6099338b
Omkar Rane <omkarrane10@gmail.com>
2020-11-04 01:56:07 +0530

    COMMONLOUNGE - minimum_difference

---
+++ COMMONLOUNGE/minimum_difference/minimum_difference.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define s_int(num) scanf("%d", &num)
+
+using namespace std;
+void solve();
+void quick_sort2(int[], int, int);
+int quick_sort_partition(int[], int, int);
+
+int main()
+{
+  int t;
+  s_int(t);
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
+}
+
+int quick_sort_partition(int arr[], int low, int high)
+{
+  int pivot = arr[high];
+  int i = low - 1;
+  for (int j = low; j < high; j++)
+  {
+    if (arr[j] <= pivot)
+    {
+      i++;
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+  i++;
+  int temp = arr[i];
+  arr[i] = arr[high];
+  arr[high] = temp;
+  return i;
+}
+
+void quick_sort2(int arr[], int low, int high)
+{
+  if (low >= high)
+    return;
+
+  int partition_index = quick_sort_partition(arr, low, high);
+  quick_sort2(arr, low, (partition_index - 1));
+  quick_sort2(arr, (partition_index + 1), high);
+}
+
+void solve()
+{
+  int n, k;
+  s_int(n);
+  s_int(k);
+  int arr[n];
+  for (int i = 0; i < n; i++)
+    s_int(arr[i]);
+
+  quick_sort2(arr, 0, n - 1);
+  int diff = INT_MAX;
+  for (int i = 0; i <= (n - k); i++)
+  {
+    int temp = arr[i + k - 1] - arr[i];
+    if (temp < diff)
+      diff = temp;
+  }
+  printf("%d\n", diff);
+}
