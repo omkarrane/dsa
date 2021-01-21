80dbb10f1d8b49d29fe507cc165a46f684fbef64
Omkar Rane <omkarrane10@gmail.com>
2021-01-22 03:17:01 +0530

    array_partition_i - LEETCODE

---
+++ LEETCODE/array_partition_i.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/array-partition-i
+class Solution
+{
+public:
+  int arrayPairSum(vector<int> &nums)
+  {
+    int sum = 0;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i += 2)
+    {
+      sum += nums[i];
+    }
+
+    return sum;
+  }
+};
