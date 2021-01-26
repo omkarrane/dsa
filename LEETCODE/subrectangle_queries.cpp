db4b394bdd04f082a9d41ffb377d5e7755a03bf8
Omkar Rane <omkarrane10@gmail.com>
2021-01-26 23:54:36 +0530

    subrectangle_queries - LEETCODE

---
+++ LEETCODE/subrectangle_queries.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/subrectangle-queries/
+class SubrectangleQueries
+{
+  vector<vector<int>> arr;
+
+public:
+  SubrectangleQueries(vector<vector<int>> &rectangle)
+  {
+    arr = rectangle;
+  }
+
+  void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
+  {
+    for (int i = row1; i <= row2; i++)
+      for (int j = col1; j <= col2; j++)
+        arr[i][j] = newValue;
+  }
+
+  int getValue(int row, int col)
+  {
+    return arr[row][col];
+  }
+};
