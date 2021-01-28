72abb305592bbb799bc2f2d8ca53c1b6e283d1aa
Omkar Rane <omkarrane10@gmail.com>
2021-01-29 02:35:59 +0530

    missing_number - LEETCODE

---
+++ LEETCODE/missing_number.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/missing-number/
+class Solution
+{
+public:
+  int missingNumber(vector<int> &nums)
+  {
+    int missing = nums.size();
+    for (int i = 0; i < nums.size(); i++)
+      missing ^= i ^ nums[i];
+
+    return missing;
+  }
+};
