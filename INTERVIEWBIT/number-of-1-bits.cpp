d40eace4b87df4427f5530716473a372dfb447ee
Omkar Rane <omkarrane10@gmail.com>
2021-02-18 04:58:18 +0530

    number-of-1-bits - INTERVIEWBIT

---
+++ INTERVIEWBIT/number-of-1-bits.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://www.interviewbit.com/problems/number-of-1-bits/
+int numSetBits(unsigned int A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int ans = 0;
+    while (A > 0) {
+        if (A & 1)
+            ans++;
+        A >>= 1;
+    }
+    
+    return ans;
+}
