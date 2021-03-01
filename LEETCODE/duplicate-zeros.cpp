d96dd031041a1ccda4d87d977340ff9e98331659
Omkar Rane <omkarrane10@gmail.com>
2021-03-01 21:39:55 +0530

    duplicate-zeros - LEETCODE

---
+++ LEETCODE/duplicate-zeros.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/duplicate-zeros/
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == 0) {
+                arr.insert(arr.begin() + i, 0);
+                i++;
+            }
+        }
+        
+        while (arr.size() != n)
+            arr.pop_back();
+    }
+};
