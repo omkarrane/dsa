22d95bb978777e27ac5934c64a03c033330feb93
Omkar Rane <omkarrane10@gmail.com>
2021-03-08 00:03:52 +0530

    koko-eating-bananas - LEETCODE

--- LEETCODE/koko-eating-bananas.cpp
+++ LEETCODE/koko-eating-bananas.cpp
@@ -2,44 +2,50 @@
 using namespace std;
 
 // https://leetcode.com/problems/koko-eating-bananas/
-class Solution {
+class Solution
+{
 public:
-    bool isPossible(vector<int> &piles, int H, long long K) {
-        if (K == 0)
-            return false;
-        int n = piles.size();
-        int current = 0;
-        long long hoursTaken = 0;
-        while (current < n) {
-            hoursTaken += (piles[current] / K);
-            if (piles[current] % K != 0)
-                hoursTaken++;
-            current++;
-        }
-        
-        return hoursTaken <= H;
+  bool isPossible(vector<int> &piles, int H, long long K)
+  {
+    if (K == 0)
+      return false;
+    int n = piles.size();
+    int current = 0;
+    long long hoursTaken = 0;
+    while (current < n)
+    {
+      hoursTaken += (piles[current] / K);
+      if (piles[current] % K != 0)
+        hoursTaken++;
+      current++;
     }
-    
-    int minEatingSpeed(vector<int>& piles, int H) {
-        int n = piles.size();
-        long long low = 1;
-        long long high = 1;
-        while (!isPossible(piles, H, high))
-            high *= 2;
-        
-        long long ans = 1;
-        while (low <= high) {
-            long long mid = low + (high - low) / 2;
-            bool possible1 = isPossible(piles, H, mid);
-            
-            if (possible1) {
-                ans = mid;
-                high = mid - 1;   
-            }
-            else
-                low = mid + 1;
-        }
-        
-        return ans;
+
+    return hoursTaken <= H;
+  }
+
+  int minEatingSpeed(vector<int> &piles, int H)
+  {
+    int n = piles.size();
+    long long low = 1;
+    long long high = 1;
+    while (!isPossible(piles, H, high))
+      high *= 2;
+
+    long long ans = 1;
+    while (low <= high)
+    {
+      long long mid = low + (high - low) / 2;
+      bool possible1 = isPossible(piles, H, mid);
+
+      if (possible1)
+      {
+        ans = mid;
+        high = mid - 1;
+      }
+      else
+        low = mid + 1;
     }
+
+    return ans;
+  }
 };
