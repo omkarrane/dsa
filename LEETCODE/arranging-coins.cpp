29d92b8f590aa4c0052a1b1a42b4bdd7e31ab1ff
Omkar Rane <omkarrane10@gmail.com>
2021-03-07 20:00:00 -0600

    arranging-coins - LEETCODE

--- LEETCODE/arranging-coins.cpp
+++ LEETCODE/arranging-coins.cpp
@@ -2,24 +2,27 @@
 using namespace std;
 
 // https://leetcode.com/problems/arranging-coins/
-class Solution {
+class Solution
+{
 public:
-    int arrangeCoins(int n) {
-        long long low = 1;
-        long long high = INT_MAX;
-        while (low <= high) {
-            long long mid = low + (high - low) / 2;
-            long long sum1 = mid * (mid + 1) / 2;
-            long long sum2 = (mid + 1) * (mid + 2) / 2;
-            
-            if (sum1 <= n && sum2 > n)
-                return mid;
-            else if (sum1 <= n)
-                low = mid + 1;
-            else
-                high = mid - 1;
-        }
-        
-        return 0;
+  int arrangeCoins(int n)
+  {
+    long long low = 1;
+    long long high = INT_MAX;
+    while (low <= high)
+    {
+      long long mid = low + (high - low) / 2;
+      long long sum1 = mid * (mid + 1) / 2;
+      long long sum2 = (mid + 1) * (mid + 2) / 2;
+
+      if (sum1 <= n && sum2 > n)
+        return mid;
+      else if (sum1 <= n)
+        low = mid + 1;
+      else
+        high = mid - 1;
     }
+
+    return 0;
+  }
 };
