db6c6a0aae097b5a5c97ce5fe6d51979ada66658
Omkar Rane <omkarrane10@gmail.com>
2021-04-02 16:20:41 +0530

    MMASS - SPOJ

--- SPOJ/MMASS.cpp
+++ SPOJ/MMASS.cpp
@@ -1,45 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int get_weight(char c) {
-        if(c == 'C') return 12;
-        else if(c == 'H') return 1;
-        else if(c == 'O') return 16;
+int get_weight(char c)
+{
+  if (c == 'C')
+    return 12;
+  else if (c == 'H')
+    return 1;
+  else if (c == 'O')
+    return 16;
 }
 
-int main() {
-        string s;
-        cin >> s;
-        stack<int> st;
-        for(int i = 0; i < s.size(); i++) {
-                if(s[i] == 'C' || s[i] == 'H' || s[i] == 'O') 
-                        st.push(get_weight(s[i]));
-                else if(s[i] == '(')
-                        st.push(0);
-                else if(s[i] == ')') {
-                        int sum = 0;
-                        while(st.top() != 0) {
-                            sum += st.top();
-                            st.pop();
-                        }
-                        st.pop();
-                        st.push(sum);
-                }
-                else {
-                        int t = (int)s[i] - 48;
-                        while(t > 1) {
-                                st.push(st.top());
-                                t--;
-                        }
-                }
-        }
+int main()
+{
+  string s;
+  cin >> s;
+  stack<int> st;
+  for (int i = 0; i < s.size(); i++)
+  {
+    if (s[i] == 'C' || s[i] == 'H' || s[i] == 'O')
+      st.push(get_weight(s[i]));
+    else if (s[i] == '(')
+      st.push(0);
+    else if (s[i] == ')')
+    {
+      int sum = 0;
+      while (st.top() != 0)
+      {
+        sum += st.top();
+        st.pop();
+      }
+      st.pop();
+      st.push(sum);
+    }
+    else
+    {
+      int t = (int)s[i] - 48;
+      while (t > 1)
+      {
+        st.push(st.top());
+        t--;
+      }
+    }
+  }
 
-        int res = 0;
-        while(!st.empty()) {
-                res += st.top();
-                st.pop();
-        }
+  int res = 0;
+  while (!st.empty())
+  {
+    res += st.top();
+    st.pop();
+  }
 
-        cout << res;
-        return 0;
+  cout << res;
+  return 0;
 }
