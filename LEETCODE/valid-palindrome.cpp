8aeb6a3346c4f3076317c480fb2ecf87b82cbcb7
Omkar Rane <omkarrane10@gmail.com>
2021-04-03 22:57:45 +0530

    valid-palindrome - LEETCODE

---
+++ LEETCODE/valid-palindrome.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/valid-palindrome/
+/**
+ * we can solve this using 2 pointers approach
+ */
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (!isalnum(s[i]))
+                i++;
+            else if (!isalnum(s[j]))
+                j--;
+            else if (tolower(s[i]) != tolower(s[j]))
+                return false;
+            else {
+                i++;
+                j--;
+            }
+        }
+        
+        return true;
+    }
+};
