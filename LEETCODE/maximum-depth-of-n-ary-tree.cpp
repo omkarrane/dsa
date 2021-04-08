664617c8233435acb78cd2fc17ad30f0c5e91525
Omkar Rane <omkarrane10@gmail.com>
2021-04-07 20:30:00 -0600

    maximum-depth-of-n-ary-tree - LEETCODE

---
+++ LEETCODE/maximum-depth-of-n-ary-tree.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (root == NULL)
+            return 0;
+        
+        int current = 1;
+        for (int i = 0; i < root->children.size(); i++) {
+            int temp = maxDepth(root->children[i]) + 1;
+            current = max(current, temp);
+        }
+        
+        return current;
+    }
+};
