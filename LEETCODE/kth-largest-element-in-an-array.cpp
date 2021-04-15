24aa975c9d002dd04f25435bd4d3da24b204f5bb
Omkar Rane <omkarrane10@gmail.com>
2021-04-15 23:48:45 +0530

    kth-largest-element-in-an-array - LEETCODE

---
+++ LEETCODE/kth-largest-element-in-an-array.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/kth-largest-element-in-an-array/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end(), greater<int>());
+        return nums[k - 1];
+    }
+};
