beb37ece4bc8350aaefa13ce8d903bbab7b4b468
Omkar Rane <omkarrane10@gmail.com>
2021-05-12 16:17:37 +0530

    largest-square-formed-in-a-matrix - GEEKSFORGEEKS

---
+++ GEEKSFORGEEKS/largest-square-formed-in-a-matrix.cpp
@@ -0,0 +1,53 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int maxSquare(int n, int m, vector<vector<int>> mat){
+        // code here
+        int dp[n + 1][m + 1];
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 0;
+            
+        for (int j = 0; j <= m; j++)
+            dp[0][j] = 0;
+        
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = 0;
+                if (mat[i - 1][j - 1] == 1)
+                    dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                
+                
+                ans = max(ans, dp[i][j]);
+            }
+        }
+        
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, m;
+        cin>>n>>m;
+        vector<vector<int>> mat(n, vector<int>(m, 0));
+        for(int i = 0;i < n*m;i++)
+            cin>>mat[i/m][i%m];
+        
+        Solution ob;
+        cout<<ob.maxSquare(n, m, mat)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
