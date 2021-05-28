6859a4b00500943f98dd07aae6754d9535aa8d01
Omkar Rane <omkarrane10@gmail.com>
2021-05-27 20:00:00 -0600

    can-place-flowers - LEETCODE

--- LEETCODE/can_place_flowers.cpp
+++ LEETCODE/can_place_flowers.cpp
@@ -2,30 +2,37 @@
 using namespace std;
 
 // https://leetcode.com/problems/can-place-flowers/
-class Solution
-{
+/**
+ * we can just simulate the process and see if n == 0
+ */
+class Solution {
 public:
-  bool canPlaceFlowers(vector<int> &flowerbed, int n)
-  {
-    for (int i = 0; i < flowerbed.size(); i++)
-    {
-      if (flowerbed[i] == 1)
-        i++;
-      else if (flowerbed[i] == 0)
-      {
-        bool p = true;
-        if (i > 0 && flowerbed[i - 1] == 1)
-          p = false;
-        if (i < flowerbed.size() - 1 && flowerbed[i + 1] == 1)
-          p = false;
-        if (p)
-        {
-          i++;
-          n--;
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        if (n == 0)
+            return true;
+        
+        if (flowerbed.size() == 1)
+            return (flowerbed[0] == 0 && n == 1);
+        
+        for (int i = 0; i < flowerbed.size(); i++) {
+            if (i == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+            else if (i == flowerbed.size() - 1 && flowerbed[i] == 0 && flowerbed[i - 1] == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+            else if (i > 0 && flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+            
+            
+            if (n == 0)
+                return true;
         }
-      }
+        
+        return n == 0;
     }
-
-    return n <= 0;
-  }
 };
